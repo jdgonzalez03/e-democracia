1bae84af3c7d9c839f3b08d9cfd5976d4eb6da94
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2024-05-22 00:16:38 -0500

    Voting form styles desing for mobile

--- src/components/Candidate.jsx
+++ src/components/Candidate.jsx
@@ -3,6 +3,20 @@ import "./Candidate.css";
 const Candidate = ({ candidate, onSelect }) => {
   return (
     <div className="candidate">
+      <label htmlFor={candidate.name}>
+        <div className="candidate-image">
+          <img src={candidate.urlImage} alt={`${candidate.name}`} />
+        </div>
+        <div className="candidate-info">
+          <h3 className="candidate-nombre">{candidate.name}</h3>
+          <p className={`partido ${candidate.color}`}>
+            Partido: {candidate.party}
+          </p>
+          <p>Edad: {candidate.age}</p>
+        </div>
+        <p className="candidate-slogan">Lema: {candidate.slogan}</p>
+      </label>
+
       <input
         type="radio"
         id={candidate.name}
@@ -10,13 +24,6 @@ const Candidate = ({ candidate, onSelect }) => {
         value={candidate.name}
         onChange={() => onSelect(candidate.name)}
       />
-      <label htmlFor={candidate.name}>
-        <img src={candidate.image} alt={`${candidate.name}`} />
-        <h3>{candidate.name}</h3>
-        <p>Party: {candidate.party}</p>
-        <p>Age: {candidate.age}</p>
-        <p>Slogan: {candidate.slogan}</p>
-      </label>
     </div>
   );
 };
